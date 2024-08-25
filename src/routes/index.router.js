bf3b04cb4c06f857e9217d06ea00e03194f09f0f
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2024-08-25 14:09:06 -0300

    Paginacion Index

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -3,28 +3,39 @@ import { ProductModel } from "../model/producto.model.js";
 
 const router = Router()
 
-router.get('/', async(req, res) => {
-
+router.get('/', async (req, res) => {
     try {
-        const { limit } = req.query;
-        const queryLimit = limit ? Number(limit) : 10; 
+        const { limit = 10, page = 1 } = req.query;
+        const queryLimit = parseInt(limit, 10);
+        const queryPage = parseInt(page, 10);
 
-        if (isNaN(queryLimit) || queryLimit <= 0) {
-            return res.status(400).json({ status: 'error', message: 'El límite debe ser un número positivo.' });
+        if (isNaN(queryLimit) || queryLimit <= 0 || isNaN(queryPage) || queryPage <= 0) {
+            return res.status(400).json({ status: 'error', message: 'Límite y página deben ser números positivos.' });
         }
 
-        const productList = await ProductModel.find().limit(queryLimit).lean();
-        
-        //console.log(productList)
-        res.render('index', { productList})
+        const result = await ProductModel.paginate({}, { 
+            limit: queryLimit, 
+            page: queryPage,
+            lean: true // Esto convierte los documentos de Mongoose a objetos de JavaScript simples
+        });
 
+        const prevLink = result.hasPrevPage ? `/?limit=${queryLimit}&page=${result.prevPage}` : null;
+        const nextLink = result.hasNextPage ? `/?limit=${queryLimit}&page=${result.nextPage}` : null;
+
+        res.render('index', { 
+            productList: result.docs, 
+            totalPages: result.totalPages,
+            page: result.page,
+            hasPrevPage: result.hasPrevPage,
+            hasNextPage: result.hasNextPage,
+            prevLink,
+            nextLink
+        });
 
     } catch (error) {
-        console.error('Error al obtener los carritos:', error);
-        res.status(500).json({ status: 'error', message: 'Error al obtener los carritos', error: error.message });
+        console.error('Error al obtener los productos:', error);
+        res.status(500).json({ status: 'error', message: 'Error al obtener los productos', error: error.message });
     }
-
-    
-})
+});
 
 export default router;
